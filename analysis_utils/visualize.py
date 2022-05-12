39885e868ea43b053309469853275be69ad1acee
AIasd <zhongziyuan1995@gmail.com>
2022-05-12 14:25:07 -0400

    update vis on dataset

--- analysis_utils/visualize.py
+++ analysis_utils/visualize.py
@@ -61,47 +61,64 @@ def plot_subplot(ax, x_list, y_list, left, right, unique_y_list, legend):
     ax.set_title('samples '+str(left)+' to '+str(right), fontsize=20)
     if legend:
         ax.legend(loc='lower right', prop={'size': 16}, fancybox=True, framealpha=0.5)
-# -------------------- helper functions for visualize_data --------------------
 
-def visualize_data(folder_path, num_subplots, mode, dim, chosen_labels):
+# extract data from result folder
+def extract_data_from_fuzzing(folder_path):
     data_path = os.path.join(folder_path, 'data.pickle')
     with open(data_path, 'rb') as f_in:
         data_d = pickle.load(f_in)
 
     x_list = data_d['x_list']
     y_list = data_d['y_list']
-    labels = np.array(data_d['labels'])
-    print('all labels', labels)
+    x_labels = np.array(data_d['labels'])
+    print('all x_labels', x_labels)
 
     xl = data_d['xl']
     xu = data_d['xu']
     used_labels_inds = xu - xl > 0
 
     x_list = x_list[:, used_labels_inds]
-    labels = labels[used_labels_inds]
-    print('used labels', labels)
+    x_labels = x_labels[used_labels_inds]
+    print('used x_labels', x_labels)
 
+    return x_list, y_list, x_labels
+
+def extract_data_from_csv(folder_path, filename, x_labels, y_label):
+    import pandas
+    df = pandas.read_csv(os.path.join(folder_path, filename))
+
+    x_list = df[x_labels].to_numpy()
+    y_list = df[y_label].to_numpy()
+    # print('x_list.shape', x_list.shape, 'y_list.shape', y_list.shape)
+
+    return x_list, y_list, np.array(x_labels)
+
+
+# -------------------- helper functions for visualize_data --------------------
+def visualize_data(save_folder_path, x_list, y_list, x_labels, num_subplots, mode, dim, chosen_labels):
     # normalize the data first
     from sklearn.preprocessing import MinMaxScaler
     transformer = MinMaxScaler().fit(x_list)
     x_list = transformer.transform(x_list)
 
+    assert num_subplots >= 1
     assert x_list.shape[0] >= num_subplots
 
     if mode == 'plain':
         assert len(chosen_labels) == dim
         inds_list = []
         for chosen_label in chosen_labels:
-            assert chosen_label in labels
-            inds_list.append(np.where(labels==chosen_label)[0][0])
+            assert chosen_label in x_labels
+            inds_list.append(np.where(x_labels==chosen_label)[0][0])
     else:
         # dimensionality reduction is only used when input dimension is larger than the visualization dimension
         assert x_list.shape[1] > dim
 
         if mode == 'pca':
             from sklearn.decomposition import PCA
-            pca = PCA(n_components=dim)
+            pca = PCA(n_components=dim, svd_solver='full')
             pca.fit(x_list)
+            print('dim', dim, 'pca.explained_variance_ratio_', pca.explained_variance_ratio_)
             x_list = pca.transform(x_list)
             inds_list = [i for i in range(dim)]
         elif mode == 'tsne':
@@ -115,7 +132,6 @@ def visualize_data(folder_path, num_subplots, mode, dim, chosen_labels):
             raise
 
     print('mode', mode, 'dim', dim, 'chosen_labels', chosen_labels)
-    print(data_d.keys())
     print('x_list.shape', x_list.shape)
     print('y_list.shape', y_list.shape)
 
@@ -126,57 +142,75 @@ def visualize_data(folder_path, num_subplots, mode, dim, chosen_labels):
 
     num_subplots_col_num = int(np.ceil(np.sqrt(num_subplots)))
     num_subplots_row_num = int(np.ceil(num_subplots / num_subplots_col_num))
-
     # for the overall plot at the first row
-    num_subplots_col_num += 1
-
-    fig, axs = plt.subplots(num_subplots_row_num, num_subplots_col_num, figsize=(num_subplots_col_num*5, num_subplots_row_num*5))
+    num_subplots_row_num += 1
 
 
-    plot_subplot(axs[0, 0], x_list, y_list, 0, x_list.shape[0], unique_y_list, legend=True)
+    if num_subplots == 1:
+        fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+        plot_subplot(ax, x_list, y_list, 0, x_list.shape[0], unique_y_list, legend=True)
+    else:
+        fig, axs = plt.subplots(num_subplots_row_num, num_subplots_col_num, figsize=(num_subplots_col_num*5, num_subplots_row_num*5))
+        plot_subplot(axs[0, 0], x_list, y_list, 0, x_list.shape[0], unique_y_list, legend=True)
 
-    # remove empty subplots at the rest of the first row
-    for i in range(1, num_subplots_col_num):
-        axs[0, i].remove()
+        # remove empty subplots at the rest of the first row
+        for i in range(1, num_subplots_col_num):
+            axs[0, i].remove()
 
-    for i in range(num_subplots):
-        cur_row = (i // num_subplots_col_num)+1
-        cur_col = i % num_subplots_col_num
-        ax = axs[cur_row, cur_col]
+        for i in range(num_subplots):
+            cur_row = (i // num_subplots_col_num)+1
+            cur_col = i % num_subplots_col_num
+            ax = axs[cur_row, cur_col]
 
-        left, right = i*num_per_subplot, (i+1)*num_per_subplot
-        if i == num_subplots-1:
-            right = x_list.shape[0]
-        plot_subplot(ax, x_list, y_list, left, right, unique_y_list, legend=False)
+            left, right = i*num_per_subplot, (i+1)*num_per_subplot
+            if i == num_subplots-1:
+                right = x_list.shape[0]
+            plot_subplot(ax, x_list, y_list, left, right, unique_y_list, legend=False)
 
-    # remove empty subplots in the end
-    axs = axs.flat
-    for i in range(num_subplots+num_subplots_col_num, len(axs)):
-        axs[i].remove()
+        # remove empty subplots in the end
+        axs = axs.flat
+        for i in range(num_subplots+num_subplots_col_num, len(axs)):
+            axs[i].remove()
 
-    fig.suptitle(mode+' with '+str(dim)+' dimensions for '+str(x_list.shape[0])+' samples', fontsize=30)
-    fig.savefig(os.path.join(folder_path, mode+'_'+str(dim)+'_'+str(x_list.shape[0])+'.jpg'))
+    if num_subplots > 1:
+        fig.suptitle(mode+' with '+str(dim)+' dimensions for '+str(x_list.shape[0])+' samples', fontsize=30)
+
+    fig.savefig(os.path.join(save_folder_path, mode+'_'+str(dim)+'_'+str(x_list.shape[0])+'.jpg'))
 
 
 if __name__ == '__main__':
-    # The folder consists of the fuzzing data
-    folder_path = 'no_simulation_function_script/run_results_no_simulation/nsga2/four_modes/2022_05_09_18_03_17,50_10_none_500_coeff_0_0.1_0.5_only_unique_0'
+
+    # -------------------- Dataset Visualization Parameters--------------------
+    folder_path = 'no_simulation_dataset_script'
+    file_name = 'grid.csv'
+    # The values with these labels will be extracted
+    x_labels = ['ego_pos', 'ego_init_speed', 'other_pos', 'other_init_speed', 'ped_delay', 'ped_init_speed']
+    # The interested target's label
+    y_label = 'oob'
+    x_list, y_list, x_labels = extract_data_from_csv(folder_path, file_name, x_labels, y_label)
+
+
+    # -------------------- Fuzzing + Visualization Parameters --------------------
+    # folder_path = 'no_simulation_function_script/run_results_no_simulation/nsga2/four_modes/2022_05_09_18_03_17,50_10_none_500_coeff_0_0.1_0.5_only_unique_0'
     # folder_path = 'carla_lbc/run_results/nsga2/town07_front_0/go_straight_town07_one_ped/lbc/2022_05_09_23_07_38,50_10_none_500_coeff_0.0_0.1_0.5_only_unique_0'
+    # x_list, y_list, labels = extract_data_from_folder(folder_path)
 
-    # folder_path = 'carla_lbc/run_results/nsga2-un/town07_front_0/go_straight_town07_one_ped/lbc/2022_05_09_23_42_33,50_10_adv_nn_500_coeff_0.0_0.1_0.5_only_unique_1'
 
-    # The number of subsets to split all the data during fuzzing. It needs to be an integer and less than or equal to (usually far less than) the number of data points.
-    num_subplots = 10
+    # -------------------- Common Parameters --------------------
+
+    # The number of subsets to split all the data during fuzzing. It needs to be a positive integer and less than or equal to (usually far less than) the number of data points. When it is set to 1, only a plot with all the data points will be shown.
+    num_subplots = 1
 
     # The visualization method. ['plain', 'pca', 'tsne']
     mode = 'plain'
 
     # The number of dimensions to visualize. For 'plain', 2 to 4 are supported and dim must be equal to len(chosen_labels); For 'pca', 2 to 4 are supported; for 'tsne', only 2 is supported
-    dim = 2
+    dim = 4
     # dim = 4
 
     # The labels used for visualization. It is used only if mode == 'plain' and every label in the chosen_labels must be in labels
-    chosen_labels = ['x1', 'x2']
+    chosen_labels = ['ego_pos', 'ego_init_speed', 'other_pos', 'other_init_speed']
+    # chosen_labels = ['x1', 'x2']
     # chosen_labels = ['pedestrian_x_0', 'pedestrian_y_0', 'pedestrian_yaw_0', 'pedestrian_speed_0']
 
-    visualize_data(folder_path, num_subplots, mode, dim, chosen_labels)
+    visualize_data(folder_path, x_list, y_list, x_labels, num_subplots, mode, dim, chosen_labels)
--- doc/visualization.md
+++ doc/visualization.md
@@ -1,20 +1,53 @@
 # Visualization
-## General Routine
-Set folder path and visualization parameters at the end of `analysis_utils/visualize.py` based on the corresponding comments.
 
+## Visualize a Given Dataset
+### Oracle
+A dataset in csv format. In this example, we use `no_simulation_dataset_script/grid.csv`.
+
+### Parameters
+#### Dataset Visualization Parameters
+Set `folder_path`, `file_name`, `x_labels`, `y_label`, and visualization parameters at the end of `analysis_utils/visualize.py` based on their corresponding comments.
+
+#### Common Parameters
+```
+num_subplots = 1
+mode = 'plain'
+dim = 4
+chosen_labels = ['ego_pos', 'ego_init_speed', 'other_pos', 'other_init_speed']
+```
+
+### Commands
 Then, run
 ```
 python analysis_utils/visualize.py
 ```
 
-## Example 1 - No Simulation (Function)
+The result is shown below:
+
+<img src="figures/plain_4_17280_dataset_airsim.jpg" width="50%" height="50%"/>
+
+
+## Fuzzing + Visualization - No Simulation (Function)
 ### Oracle
 An synthetic function `four_modes` (defined in `no_simulation_function_script/synthetic_functions.py`) is used as the oracle.
 
 ### Search Space
 ```
-x1: -1, 1
-x2: -1, 1
+x1: [-1, 1]
+x2: [-1, 1]
+```
+
+### Parameters
+#### Fuzzing + Visualization Parameters
+In `analysis_utils/visualize.py`, change `folder_path` to the folder containing the fuzzing result. In this case, it should have a path similar to `no_simulation_function_script/run_results_no_simulation/nsga2/four_modes/<specific folder name with fuzzing starting time>'`.
+
+#### Common Parameters
+Make sure `chosen_labels` in `analysis_utils/visualize.py` is a subset of `scenario_labels` in `ga_fuzzing.py` (under the elif block of 'no_simulation_function').
+```
+num_subplots = 10
+mode = 'plain'
+dim = 2
+chosen_labels = ['x1', 'x2']
 ```
 
 ### Commands
@@ -23,22 +56,17 @@ Run fuzzing for a specified function using an algorithm one wants to use based o
 python ga_fuzzing.py --simulator no_simulation_function --n_gen 10 --pop_size 50 --algorithm_name nsga2 --has_run_num 500 --n_offsprings 200 --only_run_unique_cases 0 --use_unique_bugs 0 --synthetic_function four_modes
 ```
 
-In `analysis_utils/visualize.py`, change `folder_path` to the folder containing the fuzzing result. In this case, it should have a path similar to `no_simulation_function_script/run_results_no_simulation/nsga2/four_modes/<specific folder name with fuzzing starting time>'`.
-
-Make sure `chosen_labels` in `analysis_utils/visualize.py` is a subset of `scenario_labels` in `ga_fuzzing.py` (under the elif block of 'no_simulation_function'). In this case, `chosen_labels = ['x1', 'x2']`
-
-For details of changing other visualization parameters, check out the comments in at the end of `analysis_utils/visualize.py`.
-
 Then, run
 ```
 python analysis_utils/visualize.py
 ```
 
 The resulted figure is saved in the `folder_path`. The generated figure is shown below:
-![plain_2_500_ga_four_modes](figures/plain_2_500_ga_four_modes.jpg)
+
+<img src="figures/plain_2_500_ga_four_modes.jpg" width="100%" height="100%"/>
 
 
-## Example 2 - CARLA + LBC
+## Fuzzing + Visualization - CARLA + LBC
 ### Oracle
 The stack [CARLA0.9.9+LBC](https://github.com/AIasd/ADFuzz/blob/main/doc/stack1_carla_lbc.md) is used.
 
@@ -46,14 +74,27 @@ The stack [CARLA0.9.9+LBC](https://github.com/AIasd/ADFuzz/blob/main/doc/stack1_
 The search space is a logical space about a pedestrian crossing the street starting from a point within a square region of the an intersection. The ego car is passing through the intersection.
 
 The scenario looks like the following:
-![lbc_crossing_scenario](figures/lbc_crossing_scenario.jpg)
+
+<img src="figures/lbc_crossing_scenario.jpg" width="50%" height="50%"/>
 
 The search space is:
 ```
-pedestrian_x: -12, 12
-pedestrian_y: -12, 12
-pedestrian_yaw: 0, 360
-pedestrian_speed: 0, 3
+pedestrian_x: [-12, 12]
+pedestrian_y: [-12, 12]
+pedestrian_yaw: [0, 360]
+pedestrian_speed: [0, 3]
+```
+
+### Parameters
+#### Fuzzing + Visualization Parameters
+In `analysis_utils/visualize.py`, change `folder_path` to the folder containing the fuzzing result. In this case, it should have a path similar to `no_simulation_function_script/run_results_no_simulation/<algorithm_name>/town07_front_0/go_straight_town07_one_ped/lbc/<specific folder name with fuzzing starting time>'`.
+
+#### Common Parameters
+```
+num_subplots = 10
+mode = 'plain'
+dim = 4
+chosen_labels = ['ego_pos', 'ego_init_speed', 'other_pos', 'other_init_speed']
 ```
 
 ### Commands
@@ -62,8 +103,6 @@ pedestrian_speed: 0, 3
 python ga_fuzzing.py -p 2015 --n_gen 10 --pop_size 50 -r 'town07_front_0' -c 'go_straight_town07_one_ped' --algorithm_name nsga2 --has_run_num 500 --objective_weights -1 1 1 0 0 0 0 0 0 0 --check_unique_coeff 0 0.1 0.5 --record_every_n_step 5 --debug 0 --only_run_unique_cases 0
 ```
 
-In `analysis_utils/visualize.py`, change `folder_path` to the folder containing the fuzzing result. In this case, it should have a path similar to `no_simulation_function_script/run_results_no_simulation/nsga2/town07_front_0/go_straight_town07_one_ped/lbc/<specific folder name with fuzzing starting time>'`. Set `dim=4` and the `chosen_labels` to `['pedestrian_x_0', 'pedestrian_y_0', 'pedestrian_yaw_0', 'pedestrian_speed_0']`
-
 Then, run
 ```
 python analysis_utils/visualize.py
@@ -71,7 +110,7 @@ python analysis_utils/visualize.py
 
 The result is shown below:
 
-![plain_4_500_ga_lbc](figures/plain_4_500_ga_lbc.jpg)
+<img src="figures/plain_4_500_ga_lbc.jpg" width="100%" height="100%"/>
 
 
 #### AutoFuzz
@@ -79,8 +118,6 @@ The result is shown below:
 python ga_fuzzing.py -p 2015 --n_gen 10 --pop_size 50 -r 'town07_front_0' -c 'go_straight_town07_one_ped' --algorithm_name nsga2-un --has_run_num 500 --objective_weights -1 1 1 0 0 0 0 0 0 0 --check_unique_coeff 0 0.1 0.5 --record_every_n_step 5 --debug 0 --rank_mode adv_nn
 ```
 
-In `analysis_utils/visualize.py`, change `folder_path` to the folder containing the fuzzing result. In this case, it should have a path similar to `no_simulation_function_script/run_results_no_simulation/nsga2-un/town07_front_0/go_straight_town07_one_ped/lbc/<specific folder name with fuzzing starting time>'`. Set `dim=4` and the `chosen_labels` to `['pedestrian_x_0', 'pedestrian_y_0', 'pedestrian_yaw_0', 'pedestrian_speed_0']`
-
 Then, run
 ```
 python analysis_utils/visualize.py
@@ -88,4 +125,4 @@ python analysis_utils/visualize.py
 
 The result is shown below:
 
-![plain_4_500_autofuzz_lbc](figures/plain_4_500_autofuzz_lbc.jpg)
+<img src="figures/plain_4_500_autofuzz_lbc.jpg" width="100%" height="100%"/>
